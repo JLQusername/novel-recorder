49918b085b3796ebd8390f97c6f8bb390d1fb352
B907 <1841129348@qq.com>
2025-08-16 23:09:15 +0800

    refactor(database): 重命名变量并添加获取最新阅读时间功能

--- database.py
+++ database.py
@@ -34,37 +34,31 @@ def delete_novel(id: int) -> None:
             session.commit()
 
 
-def update_novel(id: int, _novel: Novel) -> None:
+def update_novel(id: int, novel: Novel) -> None:
     with Session(engine) as session:
         statement = select(Novel).where(Novel.id == id)
         res = session.exec(statement)
-        novel = res.first()
-        if not novel:
+        old_novel = res.first()
+        if not old_novel:
             return
-        if _novel.id:
-            novel.id = _novel.id
-        novel.author = _novel.author
-        novel.name = _novel.name
-        novel.nationality = _novel.nationality
-        novel.read_time = _novel.read_time
-        novel.status = _novel.status
-        session.add(novel)
+        if novel.id:
+            old_novel.id = novel.id
+        old_novel.author = novel.author
+        old_novel.name = novel.name
+        old_novel.nationality = novel.nationality
+        old_novel.read_time = novel.read_time
+        old_novel.status = novel.status
+        session.add(old_novel)
         session.commit()
 
 
+def get_newest_read_time() -> int:
+    with Session(engine) as session:
+        statement = select(Novel).where(Novel.status == NovelStatus.FINISHED)
+        res = session.exec(statement)
+        novels = res.all()
+        return max([novel.read_time for novel in novels]) if novels else -1
+
+
 if __name__ == "__main__":
-    create_db_and_tables()
-    delete_novel(1)
-    update_novel(
-        3,
-        Novel(
-            id=3,
-            name="name",
-            author="author",
-            nationality="nationality",
-            read_time=2,
-            status=NovelStatus.FINISHED,
-        ),
-    )
-    novels = get_all_novels()
-    print(novels)
+    delete_novel(3)
